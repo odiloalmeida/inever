a41df276672bd9eb60109f7c1f7522956b8fe93a
Odilo <odilolima@hotmail.com>
2023-01-31 21:00:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText() {
+    var texts = [
+        'text1', 
+        'text2', 
+        'text3', 
+        'text4'];
+
+    var text = document.getElementById('text');
+    var randomIndex = Math.floor(Math.random() * texts.length);
+    text.innerHTML = texts[randomIndex];
+  }
